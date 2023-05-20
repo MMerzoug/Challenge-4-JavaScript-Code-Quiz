a95680336c4a9cd8016558f74a187a4be8b3c941
MMerzoug <monique.merzoug@icloud.com>
2023-05-19 20:31:08 -0700

    added HTML and began declaring variables in JavaScript

---
+++ assets/scipt.js
@@ -0,0 +1,13 @@
+var container = document.getElementByID ("container");
+var startQuiz = document.getElementByID ("start_quiz");
+var timerHolder = document.getElementById("timer_holder");
+var scoreHolder = document.getElementById("score_holder");
+var questionContainer = document.getElementById("question_container");
+var answerContainer = document.getElementById("answer_container");
+var allDone = document.getElementById("all_done");
+var initials = document.getElementById("initials");
+var submit = document.getElementById("submit");
+var highScores = document.getElementById("highscores");
+var score = document.getElementById("score");
+var goBack = document.getElementById("go_back");
+var clearHighscores = document.getElementById("clear_highscores");
