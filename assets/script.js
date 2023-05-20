81b471681ca7e197133941a3a0809ba0a58a13bc
MMerzoug <monique.merzoug@icloud.com>
2023-05-20 00:13:16 -0700

    added css to file and updated html

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
-var container = document.getElementById ("container");
-var startQuiz = document.getElementById ("start_quiz");
+var container = document.getElementById("container");
+var startQuiz = document.getElementById("start_quiz");
 var timerHolder = document.getElementById("timer_holder");
 var scoreHolder = document.getElementById("score_holder");
+var quizBox = document.getElementById("quiz_box");
 var questionContainer1 = document.getElementById("question_container_1");
 var questionContainer2 = document.getElementById("question_container_2");
 var questionContainer3 = document.getElementById("question_container_3");
@@ -16,8 +17,8 @@ var score = document.getElementById("score");
 var goBack = document.getElementById("go_back");
 var clearHighscores = document.getElementById("clear_highscores");
 
-document.getElementById("start_quiz").addEventListener("click", function(){
+document.getElementById("start_quiz").addEventListener("click", function () {
     container.style.display = "none";
-    questionContainer1.style.display = "block";
+    quiz_box.style.display = "block";
 });
 
