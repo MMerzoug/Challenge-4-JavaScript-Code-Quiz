241bcf27a4494411a270a47ba7b55851bf4acb33
MMerzoug <monique.merzoug@icloud.com>
2023-05-19 22:27:24 -0700

    added questions to html and Javascript

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,11 @@ var container = document.getElementById ("container");
 var startQuiz = document.getElementById ("start_quiz");
 var timerHolder = document.getElementById("timer_holder");
 var scoreHolder = document.getElementById("score_holder");
-var questionContainer = document.getElementById("question_container");
+var questionContainer1 = document.getElementById("question_container_1");
+var questionContainer2 = document.getElementById("question_container_2");
+var questionContainer3 = document.getElementById("question_container_3");
+var questionContainer4 = document.getElementById("question_container_4");
+var questionContainer5 = document.getElementById("question_container_5");
 var answerContainer = document.getElementById("answer_container");
 var allDone = document.getElementById("all_done");
 var initials = document.getElementById("initials");
@@ -14,5 +18,6 @@ var clearHighscores = document.getElementById("clear_highscores");
 
 document.getElementById("start_quiz").addEventListener("click", function(){
     container.style.display = "none";
+    questionContainer1.style.display = "block";
 });
 
